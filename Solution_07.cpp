68d9ca80b9767c34ad31c117519ba5f2063a0849
solp97 <103083705+solp97@users.noreply.github.com>
2022-06-10 16:38:26 +0900

    Update Solution_07.cpp

--- Solution_07.cpp
+++ Solution_07.cpp
@@ -3,24 +3,20 @@
 int main()
 {
 
-	int arr[5][5] = { 0 };
+	int arr[5][5] = { 0 }; //5x5 배열
 	int count = 0;
 	int i = 0;
 	int j = 0;
 
-	while (count < 1)
+	while (count < 1) //1이 될때까지 무한
 	{
-
 		for (i = 0; i < 5; i++)
 		{
-
-
 			for (j = 0; j < 5; j++)
 			{
 				int random = rand() % 25;
 				arr[i][j] = random;
-				printf("%d ", arr[i][j]);
-
+				printf("%d ", arr[i][j]); //각 배열위치마다 랜덤값
 			}
 			printf("\n");
 		}
@@ -28,7 +24,7 @@ int main()
 		printf("숫자를 입력해 주세요 : ");
 		int inputNum = 0;
 		scanf_s("%d", inputNum);
-		for (i = 0; i < 5; i++)
+		for (i = 0; i < 5; i++) //입력하면 0이 됨
 		{
 			for (j = 0; j < 5; j++)
 			{
